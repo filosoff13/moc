dac5315b7d995b2b332c1328fd4bf3af1a4d21ca
Vitalii Vitkovskyi <filosoff13@gmail.com>
2021-11-22 19:49:30 +0200

    Lesson#3: Added full_name & is_valid_email? methods

--- HomeWork3/task#1.rb
+++ HomeWork3/task#1.rb
@@ -28,4 +28,28 @@ class User
   def email=(email)
     @email = email
   end
+
+  def full_name
+    "#{self.first_name},  #{self.last_name}"
+  end
+
+  def is_valid_email?
+    result = self.email.scan(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+    if result[0] != nil
+    then
+      true
+    else
+      false
+    end
+  end
 end
+
+u1 = User.new('John', 'Doe', 'Doe.Johngmailgmail.com')
+puts "First name: #{u1.first_name}" # => John
+puts "Last name: #{u1.last_name}" # =>
+puts "Email: #{u1.email}" # => Doe.Johngmailgmail.com
+puts "Full name: #{u1.full_name}" # => John Do
+puts "Valid_email: #{u1.is_valid_email?}" # => false
+# puts "User is valid: #{u1.valid?}" # => false
+# puts "Errors: #{u1.errors}" # => ["last_name can not be empty", "last_name must be a string", "email is invalid"]
+# puts ''\
