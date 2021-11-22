7bcbc0ae08cd7307100ccde5740671b4bf9265da
Vitalii Vitkovskyi <filosoff13@gmail.com>
2021-11-22 18:21:58 +0200

    Lesson#3: Added class User

---
+++ HomeWork3/task#1.rb
@@ -0,0 +1,31 @@
+class User
+  def initialize(first_name, last_name, email)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def email
+    @email
+  end
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def email=(email)
+    @email = email
+  end
+end
