45691509f0137b0900b7bec9ec73c7e14310a0d6
Vitalii Vitkovskyi <filosoff13@gmail.com>
2021-11-29 16:53:14 +0200

    Upd HW#1

--- Home work 1/task#3.rb
+++ Home work 1/task#3.rb
@@ -4,12 +4,20 @@
 # 'football, programming, hiking'
 # Після цього, потрібно обрати випадкове хоббі та вивести в консоль наступний текст Tell me something about _random_hobby_
 # де замість _random_hobby_ повинно бути випадкове хоббі
-hobbies = Array.new
- 3.times{
-    puts 'Please, enter your hobby'
-    h = gets.chomp
-    hobbies.push(h)
-}
+
+# Список можливих варіантів діставання випадкового елементу масива
+# user_input.sample
+# user_input.shuffle.first
+# array_length = user_input.size 
+# user_input[rand(array_length)]
+
+# hobbies = Array.new
+#  3.times{
+#     puts 'Please, enter your hobby'
+#     h = gets.chomp
+#     hobbies.push(h)
+# }
+hobbies = gets.split(', ')
 
 random_hobby = hobbies.sample
 puts "Tell me something about #{random_hobby}"
